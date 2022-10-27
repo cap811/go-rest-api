6f3e14efbb1b9df11e4383a51f2cf895030c2d9b
cap811 <uni.eiubov@gmail.com>
2022-10-27 21:59:18 +0300

    Update

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"example.com/go-rest-api/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+	initializers.ConnectDB()
+}
 
 func main() {
-	fmt.Println("hello, world")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
